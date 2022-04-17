87b09824235bfec7a30070f1e407a6d57ac37817
daanial11 <daanial74@gmail.com>
2022-04-17 23:06:27 +0100

    gridYolo

--- jobs/grid-search/id_12.sh
+++ jobs/grid-search/id_12.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:4
-#SBATCH --partition mwvdk
+#SBATCH --partition gpu
 #SBATCH --job-name=grid12
 #SBATCH --mem=85000M
 
@@ -16,6 +16,6 @@ source ~/work-env/bin/activate
 
 cd ~/Transformer-YOLOX
 
-python train.py gpus='0,1,2,3' backbone="Swin-l" num_epochs=10 exp_id="grid12" fold=1 no_aug_epochs=1 save_epoch=100 use_amp=True val_intervals=2 data_num_workers=14 basic_lr_per_img=0.00001 batch_size=32 freeze_backbone=True use_amp=True random_size=[14,26] input_size=[640,640] test_size=[640,640] load_model="exp/Swin_l_pretrained_yolo_l/model_50.pth"
+python train.py gpus='0,1,2,3' backbone="Yolo-l" num_epochs=10 exp_id="grid12Yolo" fold=1 no_aug_epochs=1 save_epoch=100 use_amp=True val_intervals=2 data_num_workers=14 basic_lr_per_img=0.00001 batch_size=32 freeze_backbone=True use_amp=True random_size=[14,26] input_size=[640,640] test_size=[640,640] load_model="exp/CSP_l_pretrained_yolo_l/model_50.pth"
 
 sleep 60
---
+++ statistics/class_bar_chart.py
@@ -0,0 +1,47 @@
+import json
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+def get_cmap(n, name='hsv'):
+    '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct 
+    RGB color; the keyword argument name must be a standard mpl colormap name.'''
+    return plt.cm.get_cmap(name, n)
+
+
+with open ('new_merged.json',"r") as f:
+    data = json.load(f)
+
+
+colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
+          '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+class_labels = ['Bottle', 'Bottle cap', 'Can', 'Cigarette', 'Cup', 'Lid', 'Other', 'Plastic Bag', 'Pop tab', 'Straw']
+class_count = {}
+for i in range(10):
+    class_count[i+1] = 0
+
+
+for annotation in data['annotations']:
+    id = annotation.get('category_id')
+
+    class_count[id] += 1
+
+
+
+plt.style.use('ggplot')
+
+my_cmap = cm.get_cmap('jet')
+
+
+x_pos = [i for i, _ in enumerate(class_labels)]
+
+plt.bar(x_pos, class_count.values(), color=colors)
+plt.xlabel("Category", fontsize=16)
+plt.ylabel("Number of annotations", fontsize=16)
+
+
+plt.xticks(x_pos, class_labels, rotation=45)
+
+plt.show()
+
+    
+    
