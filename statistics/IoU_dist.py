478db5d56c95994066cfc71537c0e195553a08fc
daanial11 <daanial74@gmail.com>
2022-04-30 19:59:26 +0100

    iou

--- statistics/IoU_dist.py
+++ statistics/IoU_dist.py
@@ -1,7 +1,11 @@
+from itertools import count
 import json
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import torch
+import torchvision.ops.boxes as box
 
-
+import pycocotools.coco as coco_
+from pycocotools.cocoeval import COCOeval
 
 result_file_path = 'result_Swin-l_640.json'
 ann_path = 'instances_val2017.json'
@@ -19,6 +23,7 @@ results_dict_imgId = {}
 for result in result_data:
     
     img_id = result['image_id']
+    
 
     if img_id not in results_dict_imgId:
         results_dict_imgId[img_id] = []
@@ -38,9 +43,58 @@ for ann in ann_data['annotations']:
     
     annotation_dict_imgId[img_id].append(ann)
 
-
+preds_per_image_highest_ious = {}
 for k, v in annotation_dict_imgId.items():
-    print(len(v))
-    print(k)
-    print(v)
-    break
+    img_id = k
+
+    highest_ious = []
+    for ann in v:
+        gt_box = ann['bbox']
+
+        highest_iou = 0
+
+        predictions_for_img = results_dict_imgId[img_id]
+
+        for pred in predictions_for_img:
+            bbox = pred['bbox']
+
+            box1 = torch.tensor([[gt_box[0], gt_box[1], gt_box[2] - gt_box[0], gt_box[3] - gt_box[1]]], dtype=torch.float)
+            box2 = torch.tensor([[bbox[0], bbox[1], bbox[2] - bbox[0], bbox[3] - bbox[1]]], dtype=torch.float)
+
+            iou = box.box_iou(box1, box2)
+
+            if iou.item() > highest_iou:
+                highest_iou = iou.item()
+        
+        highest_ious.append(highest_iou)
+    
+    preds_per_image_highest_ious[img_id] = highest_ious
+
+
+ious = []
+for k,v in preds_per_image_highest_ious.items():
+    print(f"imageID {k}, preds ious: {v}")
+
+    for iou in v:    
+            if iou > 0:
+                ious.append(iou)
+
+
+plt.hist(ious, density=True, bins=30)
+
+plt.ylabel('Probability')
+plt.xlabel('Data')
+
+plt.show()
+
+
+
+
+
+
+
+        
+
+
+        
+
